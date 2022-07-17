348cfe0b79babfc0da2b989e90dc508546d10a6e
Aaditi-S <aaditinirfarake3@gmail.com>
2022-07-18 03:20:15 +0530

    heroku 8

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = 'postgres://qumvgelrwxblcz:7c2b5da21339f4594934b625de105bd1a5e1f0754b2004c422e21e1df2d49a36@ec2-3-217-14-181.compute-1.amazonaws.com:5432/dbbrc1uulrulq7'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = 'postgres://qumvgelrwxblcz:7c2b5da21339f4594934b625de105bd1a5e1f0754b2004c422e21e1df2d49a36@ec2-3-217-14-181.compute-1.amazonaws.com:5432/dbbrc1uulrulq7'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
